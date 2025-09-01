ce6fc60bd93453aca1a189cfe4c61f44343d5de8
GabiCST <gabicunesteanu@gmail.com>
2025-09-01 22:30:54 +0300

    -

--- Admin/AdminWindow.xaml.cs
+++ Admin/AdminWindow.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Event_App
 {
@@ -38,13 +26,14 @@ namespace Event_App
         private void AddTickets_Button(object sender, RoutedEventArgs e)
         {
             if (_user == null)
-    {
-        MessageBox.Show("User information is missing. Cannot add tickets.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-        return;
-    }
-    AddTickets addticket = new(_user);
-    addticket.Show();
-    this.Close();
+            {
+            MessageBox.Show("User information is missing. Cannot add tickets.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            return;
+            }
+
+            AddTickets addticket = new(_user);
+            addticket.Show();
+            this.Close();
         }
         private void AvailableTickets_Button(object sender, RoutedEventArgs e)
         { 
