f095835940bfbf1bdcfcda8eafd1f5b34e932906
Grigoruta Alexandru-Pavel <alex7dif@gmail.com>
2025-09-02 17:12:42 +0300

    ViewTickets (cu buy si favorite)

--- ViewTickets.xaml.cs
+++ ViewTickets.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -49,12 +50,33 @@ namespace Event_App
                 Margin = new Thickness(5),
                 Padding = new Thickness(10)
             };
+            Button BuyButton = new Button();
+            Button FavButton = new Button();
+            FavButton.Content = "Add to Favorites";
+            FavButton.Margin = new Thickness(5);
+            FavButton.Click += (s, e) =>
+            {
+                string FavTicket = $"{Type},{title},{date},{time},{nrTickets}";
+                File.AppendAllBytes("favorite_tickets.txt", Encoding.UTF8.GetBytes(FavTicket + Environment.NewLine));
+                MessageBox.Show($"You have added {title} to your favorites!");
+            };
+            
+            BuyButton.Content = "Buy";
+            BuyButton.Margin = new Thickness(5);
+            BuyButton.Click += (s, e) =>
+            {
+                string BoughtTicket = $"{Type},{title},{date},{time},{nrTickets}";
+                File.AppendAllBytes("bought_tickets.txt", Encoding.UTF8.GetBytes(BoughtTicket + Environment.NewLine));
+                MessageBox.Show($"You have bought a ticket for {title} on {date} at {time}");
+            };
             StackPanel panel = new();
             panel.Children.Add(new TextBlock { Text = $"Event Type:{Type}" });
             panel.Children.Add(new TextBlock { Text = $"Event: {title}", FontWeight = FontWeights.Bold });
             panel.Children.Add(new TextBlock { Text = $"Date: {date}" });
             panel.Children.Add(new TextBlock { Text = $"Time: {time}" });
             panel.Children.Add(new TextBlock { Text = $"Available Tickets: {nrTickets}" });
+            panel.Children.Add(BuyButton);
+            panel.Children.Add(FavButton);
             card.Child = panel;
             EventsPanel.Children.Add(card);
         }
